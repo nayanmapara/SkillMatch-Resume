2e3afa695d96ed582e81f899087d67913a01fb8d
Ryan Farekh <rayanfarekh@gmail.com>
2024-07-28 03:18:04 -0400

    Updated Sign up page

--- skillmatch-resume/data/config.tsx
+++ skillmatch-resume/data/config.tsx
@@ -8,7 +8,7 @@ import { Logo } from './logo'
 const siteConfig = {
   logo: Logo,
   seo: {
-    title: 'Saas UI',
+    title: 'Skill Match Resume',
     description: 'The React component library for startups',
   } as NextSeoProps,
   termsUrl: '#',
--- skillmatch-resume/pages/signup.tsx
+++ skillmatch-resume/pages/signup.tsx
@@ -1,34 +1,18 @@
-import { NextPage } from 'next'
-import NextLink from 'next/link'
-import { Box, Center, Stack, Text } from '@chakra-ui/react'
-import { Link } from '@saas-ui/react'
-import { Auth } from '@saas-ui/auth'
-import { Features } from 'components/features'
-import { BackgroundGradient } from 'components/gradients/background-gradient'
-import { Section } from 'components/section'
-import siteConfig from 'data/config'
-
-import { FaGithub, FaGoogle } from 'react-icons/fa'
-import { PageTransition } from 'components/motion/page-transition'
-
-const providers = {
-  google: {
-    name: 'Google',
-    icon: FaGoogle,
-  },
-  github: {
-    name: 'Github',
-    icon: FaGithub,
-    variant: 'solid',
-  },
-}
+import { NextPage } from 'next';
+import { Box, Center, Stack, Text } from '@chakra-ui/react';
+import { Link } from '@saas-ui/react';
+import { Auth } from '@saas-ui/auth';
+import { BackgroundGradient } from 'components/gradients/background-gradient';
+import { Section } from 'components/section';
+import siteConfig from 'data/config';
+import { PageTransition } from 'components/motion/page-transition';
 
 const Signup: NextPage = () => {
   return (
-    <Section height="100vh" innerWidth="container.xl">
+    <Section height="100vh" innerWidth="container.sm">
       <BackgroundGradient
         zIndex="-1"
-        width={{ base: 'full', lg: '50%' }}
+        width="full"
         left="auto"
         right="0"
         borderLeftWidth="1px"
@@ -38,65 +22,32 @@ const Signup: NextPage = () => {
         }}
       />
       <PageTransition height="100%" display="flex" alignItems="center">
-        <Stack
-          width="100%"
-          alignItems={{ base: 'center', lg: 'flex-start' }}
-          spacing="20"
-          flexDirection={{ base: 'column', lg: 'row' }}
-        >
-          <Box pe="20">
-            <NextLink href="/">
-              <Box
-                as={siteConfig.logo}
-                width="160px"
-                ms="4"
-                mb={{ base: 0, lg: 16 }}
-              />
-            </NextLink>
-            <Features
-              display={{ base: 'none', lg: 'flex' }}
-              columns={1}
-              iconSize={4}
-              flex="1"
-              py="0"
-              ps="0"
-              maxW={{ base: '100%', xl: '80%' }}
-              features={siteConfig.signup.features.map((feature) => ({
-                iconPosition: 'left',
-                variant: 'left-icon',
-
-                ...feature,
-              }))}
-            />
+        <Center height="100%" flex="1">
+          <Box width="full" pt="8" px="8">
+            <Auth
+              view="signup"
+              title="Join the AI Resume Optimization Revolution"
+              loginLink={<Link href="/login">Log in</Link>}
+            >
+              <Text color="muted" fontSize="sm">
+                By signing up you agree to our{' '}
+                <Link href={siteConfig.termsUrl} color="white">
+                  Terms of Service
+                </Link>{' '}
+                and{' '}
+                <Link href={siteConfig.privacyUrl} color="white">
+                  Privacy Policy
+                </Link>
+              </Text>
+            </Auth>
           </Box>
-          <Center height="100%" flex="1">
-            <Box width="container.sm" pt="8" px="8">
-              <Auth
-                view="signup"
-                title="Join the AI Resume Optimization Revolution"
-                providers={providers}
-                loginLink={<Link href="/login">Log in</Link>}
-              >
-                <Text color="muted" fontSize="sm">
-                  By signing up you agree to our{' '}
-                  <Link href={siteConfig.termsUrl} color="white">
-                    Terms of Service
-                  </Link>{' '}
-                  and{' '}
-                  <Link href={siteConfig.privacyUrl} color="white">
-                    Privacy Policy
-                  </Link>
-                </Text>
-              </Auth>
-            </Box>
-          </Center>
-        </Stack>
+        </Center>
       </PageTransition>
     </Section>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
 
 export const getStaticProps = () => {
   return {
@@ -108,5 +59,5 @@ export const getStaticProps = () => {
         borderTopWidth: '1px',
       },
     },
-  }
-}
+  };
+};
